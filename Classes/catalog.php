cff1e197f0885580b984775a5d60492fcccb3195
paspvik <preben.aspvik@gmail.com>
2019-05-12 15:14:34 +0200

    catalogadmin

--- Classes/catalog.php
+++ Classes/catalog.php
@@ -44,6 +44,52 @@ class Catalog {
                 echo "</tr>";
             }
     }
+
+
+    public function viewCatalogs() {
+        $get_catalogs = "SELECT cat_name FROM catalog";
+        
+        foreach($this->db->query($get_catalogs) as $row) {
+            echo "<option>". $row['cat_name']. "</option>";
+        }
+    }
+
+    public function viewCatalogID() {
+        $get_catalog_id = "SELECT cat_id FROM catalog";
+        
+        foreach($this->db->query($get_catalog_id) as $row) {
+            echo "<option>". $row['cat_id']. "</option>";
+        }
+    }
+
+
+    public function deleteCatalog(){
+        $cat_name = $_POST['cat_name']; // Gets filename from selectionmenu
+        $get_cat_id = "SELECT cat_id FROM catalog WHERE cat_name = '$cat_name'" ; 
+        $cat_id = $this->db->query("$get_cat_id")->fetch();
+        $cat_id = (int)$cat_id[0];
+
+        $stmt = "DELETE FROM catalog WHERE cat_id = $cat_id";
+        $this->db->query("$stmt"); // Prepare
+        
+
+
+    }
+
+
+    public function changeCatalogID(){
+
+        $cat_name = $_POST['cat_name']; // Gets filename from selectionmenu
+        $new_id  = $_POST['new_id']; // Gets tagname from selectionmenu
+
+        $new_id = (int)$new_id;
+
+        
+        $stmt = "UPDATE catalog SET cat_id = $new_id where cat_name = '$cat_name'"; // Prepare
+        $this->db->query("$stmt");
+ 
+
+    }
             
 
 }	
--- file_catalog_site/catalog_creator.php
+++ file_catalog_site/catalog_creator.php
@@ -1,27 +1,69 @@
 
 
 <?php
-    require '../Classes/catalog.php'; 
+	require '../Classes/catalog.php'; 
+	$catalog = new Catalog();
 ?>
 
 
 
-<h1> Here you can create catalogs.... </h1>
+<h1> Create catalogs</h1>
 
 <html>
 	<body>
 	<form action="catalog_creator.php" method="post" enctype="multipart/form-data">
 		Catalog Name: <input type="text" name="cat_name"><br>
-		<button type="submit" name="create"> Create </button>
+		<button type="submit" name="btn_submit" value= "create_catalog"> Create </button>
 	</form>
 	</body>
 </html>
-	<?php
-		$catalog = new Catalog();
-		$catalog->catalogCreator();
-	?>
 </div>
 
+
+
+
+<h1> Delete Catalog </h1>
+    <form action="catalog_creator.php" method="post" enctype="multipart/form-data">
+         <label>Tags:</label>
+            <label>Catalog Name:</label>
+            <select name="cat_name"> 
+            <?php
+			   $catalog->viewCatalogs();
+            ?>  
+            </select>
+            <button type="submit" name="btn_submit" value= "remove_catalog"> Delete </button>
+            </form>
+
+
+
+
+
+<h1> Change access level for catalog</h1>
+    <form action="catalog_creator.php" method="post" enctype="multipart/form-data">
+         <label>Catalog Name:</label>
+            <select name="cat_name">
+            <?php 
+                $catalog->viewCatalogs();
+            ?>  
+            </select>
+			New ID: <input type="number" name="new_id">
+            </select>
+            <button type="submit" name="btn_submit" value= "change_id"> Change ID </button>
+            </form>
+
+
+
+<?php
+    if($_REQUEST['btn_submit']=="create_catalog"){
+		$catalog->catalogCreator();
+    }
+    else if($_REQUEST['btn_submit']=="remove_catalog"){
+        $catalog->deleteCatalog();
+	}
+	else if($_REQUEST['btn_submit']=="change_id"){
+        $catalog->changeCatalogID();
+	}
+?>
 
 
 <div>
